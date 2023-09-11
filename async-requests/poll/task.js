66cff9fc412cc5e41f16bac87116c2b10714d676
Первушина Анна <annafirst14@mail.ru>
2023-09-11 12:51:58 +0500

    опрос дз

---
+++ async-requests/poll/task.js
@@ -0,0 +1,13 @@
+  const button = document.createElement("button");
+        button.textContent = element;
+        answer.appendChild(button);
+
+        button.addEventListener("click", () => {
+          alert("Спасибо, ваш голос засчитан!");
+        });
+      });
+    }
+  })
+  .catch((error) => {
+    console.error("Ошибка при получении данных:", error);
+  });
---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,35 @@
+const progress = document.getElementById('progress');
+const form = document.getElementById('form');
+
+function upload(file) {
+    const xhr = new XMLHttpRequest();
+
+    // обработчик для отправки
+    xhr.upload.onprogress = function(event) {
+        if (event.lengthComputable) {
+            progress.value = (event.loaded / event.total) * 100;
+        }
+    }
+
+    // обработчики успеха и ошибки
+    xhr.onload = xhr.onerror = function() {
+        if (this.status === 200) {
+            console.log("success");
+        } else {
+            console.log("error " + this.status);
+        }
+    };
+
+    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload", true);
+    xhr.send(file);
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const fileInput = form.elements.file;
+    const file = fileInput.files[0];
+    if (file) {
+        upload(file);
+    }
+    return false;
+});
